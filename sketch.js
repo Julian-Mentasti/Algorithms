4d99d3293495053e55153fbc28d91c6a9dd9d694
Julian Mentasti <julian.mentasti@gmail.com>
2018-03-05 20:56:06 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var tree;
+
+function setup() {
+  noCanvas();
+
+  // New tree
+  tree = new Tree();
+
+  // Add ten random values
+  for (var i = 0; i < 10; i++) {
+    tree.addValue(floor(random(0, 100)));
+  }
+
+  // Traverse the tree
+  tree.traverse();
+
+  // Search the tree for 10
+  var result = tree.search(10);
+  if (result == null) {
+    console.log('not found');
+  } else {
+    console.log(result);
+  }
+}
---
+++ tree.js
@@ -0,0 +1,26 @@
+// Tree object
+function Tree() {
+  // Just store the root
+  this.root = null;
+}
+
+// Start by visiting the root
+Tree.prototype.traverse = function() {
+  this.root.visit();
+}
+
+// Start by searching the root
+Tree.prototype.search = function(val) {
+  var found = this.root.search(val);
+  return found;
+}
+
+// Add a new value to the tree
+Tree.prototype.addValue = function(val) {
+  var n = new Node(val);
+  if (this.root == null) {
+    this.root = n;
+  } else {
+    this.root.addNode(n);
+  }
+}
